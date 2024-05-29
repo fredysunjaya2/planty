5261d119f2cbb8ece5805085af2d6ffa55bd1b9b
sterben404 <fredy.sunjaya@binus.ac.id>
2024-05-29 13:28:57 +0700

    Setup project

---
+++ planty/resources/views/components/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar fixed-top navbar-expand-lg">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex w-100 align-items-center">
+                <ul class="d-flex column-gap-5 navbar-nav mb-lg-0 ms-5 me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Plant Care</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Subscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                </ul>
+                <ul class="d-flex column-gap-5 align-items-center navbar-nav mb-lg-0 me-5">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img class="rounded-circle" src="/assets/user.png" style="height: 50px">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-100 position-absolute d-flex justify-content-center pb-3">
+                <a class="navbar-logo" href="#">
+                    <img src="/assets/planty.png" alt="Bootstrap" style="height: 75px">
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
