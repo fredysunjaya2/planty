8697bc262d7dbef8c3681097549d94db5e3e792b
kenzzhie <123430287+kenzzhie@users.noreply.github.com>
2024-06-03 16:08:42 +0700

    homepage
    jumbotron and services structure

--- planty/resources/views/index.blade.php
+++ planty/resources/views/index.blade.php
@@ -8,12 +8,51 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <!-- <link rel="stylesheet" href="/../css/style.css"> -->
   </head>
   <body>
-    <x-navbar>
-
-    </x-navbar>
+    <!-- <x-navbar>
+    </x-navbar> -->
+    <div class="container">
+      <section class="jumbo">
+        <div class="row-jumbo d-flex">
+          <div class="col-7 ">
+            <div class="jumbo-text">
+                <h1>Grow your own garden now.</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic veniam consequuntur ea ab necessitatibus c</p>
+            </div>
+            <div class="jumbo-btn d-flex">
+            <button type="button" class="btn btn-primary">Base class</button>
+            <button type="button" class="btn btn-secondary">Base class</button>
+            </div>
+          </div>
+          <div class="col-5 ">ini gambar</div>
+        </div>
+      </section>
+      <section class="services d-flex">
+        <div class="row-services d-flex flex-column">
+          <div class="row d-flex">
+            <div class="col-6">
+              <div class="services-title">
+                <h3>Our Services</h3>
+              </div>
+              <div class="services-desc">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt, dolorum sequi digni</p>
+              </div>
+            </div>
+            <div class="col-6 card-service ">
+                
+            </div>
+          </div>
+          <div class="row d-flex ">
+              <div class="col-6 card-service "></div>
+              <div class="col-6 card-service "></div>
+          </div>
+        </div>
+      </section>
+      
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
