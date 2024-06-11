6da60096971e2cee515fec64a35ef35f849bdb0d
Tasiaaaaa <anastasiam4002@gmail.com>
2024-06-11 16:35:24 +0700

    disease page

---
+++ planty/resources/views/disease.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diseases</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/disease.css') }}" />
+</head>
+<body>
+    <div class="container">
+        <div class="card d-flex">
+            <!-- <button>Back</button> -->
+            <div class="title row">
+                Plant Diseases
+            </div>
+            <div class="sub-title row">
+                4 of the most common plant diseases
+            </div>
+            <div class="disease-card d-flex">
+                <div class="card-title">
+                    1 <br>
+                    Downy Mildew
+                </div>
+                <div class="card-title">
+                    2 <br>
+                    Downy Mildew   
+                </div>
+                <div class="card-title">
+                    3 <br>
+                    Downy Mildew
+                </div>
+                <div class="card-title">
+                    4 <br>
+                    Downy Mildew
+                </div>
+            </div>
+        </div>
+
+        <div class="desc-card">
+            <div class="desc-disease-card d-flex col-2">
+                <div class="desc d-flex">
+                    <div class="image-disease">
+                        Image
+                    </div>
+                    <div class="desc-disease row">
+                        Downy Mildew
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                    </div>
+                </div>
+                <div class="desc d-flex">
+                    <div class="image-disease">
+                        Image
+                    </div>
+                    <div class="desc-disease row">
+                        Downy Mildew
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="desc-disease-card d-flex">
+                <div class="desc d-flex">
+                        <div class="image-disease">
+                            Image
+                        </div>
+                        <div class="desc-disease row">
+                            Downy Mildew
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                        </div>
+                    </div>
+                    <div class="desc d-flex">
+                        <div class="image-disease">
+                            Image
+                        </div>
+                        <div class="desc-disease row">
+                            Downy Mildew
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
