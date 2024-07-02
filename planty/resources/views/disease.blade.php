f7922f1e1ff262fd70656517f9e0c8b537c636c5
Tasiaaaaa <anastasiam4002@gmail.com>
2024-07-02 16:45:06 +0700

    DiseasePage

--- planty/resources/views/disease.blade.php
+++ planty/resources/views/disease.blade.php
@@ -11,81 +11,41 @@
     <link rel="stylesheet" type="text/css" href="{{ url('/css/disease.css') }}" />
 </head>
 <body>
-    <div class="container">
-        <div class="card d-flex">
-            <!-- <button>Back</button> -->
-            <div class="title row">
-                Plant Diseases
+    <div class="container-fluid position-relative d-flex row m-0 p-0" style="background-image: url('../assets/gradient-disease-background.png'); background-size: cover">
+        <div class="d-flex row align-items-center justify-content-center m-0 p-0" style="background-image: url('../assets/diseases-jumbotron.png'); background-size: contain; width:100%; height:100vh">
+            <a href="#">
+                <img src="../assets/Vector.svg" alt="" class="ps-5 pt-5">
+            </a>
+            <div class="title row text-center pb-5">
+                <h1 class="text-white" style="font-family: Poppins, sans-serif; font-weight:700; font-size:3rem">Plant Diseases</h1>
             </div>
-            <div class="sub-title row">
-                4 of the most common plant diseases
-            </div>
-            <div class="disease-card d-flex">
-                <div class="card-title">
-                    1 <br>
-                    Downy Mildew
+            <div class="d-flex row">
+                <div class="sub-title row text-center">
+                    <p class="text-white" style="font-family: Poppins, sans-serif; font-weight:600; font-size:2rem">4 of the most common plant diseases</p>
                 </div>
-                <div class="card-title">
-                    2 <br>
-                    Downy Mildew   
-                </div>
-                <div class="card-title">
-                    3 <br>
-                    Downy Mildew
-                </div>
-                <div class="card-title">
-                    4 <br>
-                    Downy Mildew
+                <div class="col-12 d-flex row mt-3 pb-5">
+                    <div class="col-1"></div>
+                    <div class="disease-card d-flex align-items-center justify-content-around col-10">
+                        <a href="#disease1"><x-disease-card>1</x-disease-card></a>
+                        <a href="#disease2"><x-disease-card>2</x-disease-card></a>
+                        <a href="#disease3"><x-disease-card>3</x-disease-card></a>
+                        <a href="#disease4"><x-disease-card>4</x-disease-card></a>
+                    </div>
+                    <div class="col-1"></div>
                 </div>
             </div>
         </div>
 
-        <div class="desc-card">
-            <div class="desc-disease-card d-flex col-2">
-                <div class="desc d-flex">
-                    <div class="image-disease">
-                        Image
-                    </div>
-                    <div class="desc-disease row">
-                        Downy Mildew
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                    </div>
-                </div>
-                <div class="desc d-flex">
-                    <div class="image-disease">
-                        Image
-                    </div>
-                    <div class="desc-disease row">
-                        Downy Mildew
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                    </div>
-                </div>
+        <div class="desc-card d-flex position-relative row col-12" style="top: 10rem">
+            <div class="desc-disease-card d-flex col-6">
+                <x-description-disease id="disease1"></x-description-disease>
+                <x-description-disease id="disease2"></x-description-disease>
             </div>
 
-            <div class="desc-disease-card d-flex">
-                <div class="desc d-flex">
-                        <div class="image-disease">
-                            Image
-                        </div>
-                        <div class="desc-disease row">
-                            Downy Mildew
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                        </div>
-                    </div>
-                    <div class="desc d-flex">
-                        <div class="image-disease">
-                            Image
-                        </div>
-                        <div class="desc-disease row">
-                            Downy Mildew
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
-                        </div>
-                    </div>
-                </div>
+            <div class="desc-disease-card d-flex col-6">
+                <x-description-disease id="disease3"></x-description-disease>
+                <x-description-disease id="disease4"></x-description-disease>
+            </div>
         </div>
     </div>
 </body>
