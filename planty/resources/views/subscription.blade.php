847c9822a910db96560956144b80e2508f32fc0c
d4rkyow <130200771+d4rkyow@users.noreply.github.com>
2024-07-11 19:33:35 +0700

    accordion controller, benefit controller , change style.css  , perbaikan subscription and funfact

--- planty/resources/views/subscription.blade.php
+++ planty/resources/views/subscription.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/utils.css') }}" />
 
   </head>
   <body>
@@ -26,31 +27,31 @@
             <div class="row  justify-content-center subscription-plan">
                 <div class="col-4 subscription-card">
                 <div class="card-body-subs">
-                    <h2 class="subscription-title ">Begineer</h2>
-                    <h2 class="planty-heading-4-subs subscription-price mb-2 ">Rp 39.299,00</h2>
-                    <p class="benefit-desc planty-text-content">per active user, per month</p>
+                    <h2 class="planty-heading-2 subscription-title ">Beginner</h2>
+                    <h2 class="planty-heading-4 subscription-price mb-2 ">Rp 39.299,00</h2>
+                    <p class="benefit-desc planty-text-sentence">per active user, per month</p>
                     <div class="btn-plan"><button type="button" class="btn btn-success">Select Plan</button></div>
                 </div>
             </div>
 
             <div class="col-4 subscription-card">
                 <div class="card-body-subs">
-                    <h2 class="subscription-title ">Enthusiast</h2>
-                    <h1 class="planty-heading-4-subs subscription-price mb-2 ">Rp 79.299,00</h1>
-                    <p class="benefit-desc planty-text-content">per active user, per month </p>
+                    <h2 class=" planty-heading-2 subscription-title ">Enthusiast</h2>
+                    <h1 class="planty-heading-4 subscription-price mb-2 ">Rp 79.299,00</h1>
+                    <p class="benefit-desc planty-text-sentence ">per active user, per month </p>
                     <div class="btn-plan"><button type="button" class="btn btn-success ">Select Plan</button></div>
                 </div>
                 </div>
             </div>
             {{-- bawah harga--}}
-            <div class="row position-relative plan-information-content">
+            <div class="row position-relative plan-information-content ">
 
-            <div class="col-5 plan-information-1">
-                <div class="subs-list">
+            <div class="col-5 plan-information-1 p-0 m-0">
+                <div class="subs-list px-5">
                     <div class="plan-information-title-1">
-                        <p class="planty-text-content">start your plant journey with easy- <br> to-care-for plants and comprehensive guidance.</p>
+                        <p class="planty-text-sentence ">start your plant journey with easy- <br> to-care-for plants and comprehensive guidance.</p>
                     </div>
-                    <ul>
+                    <ul class="planty-text-sentence">
                         <li>&#10004; Monthly Plant Delivery: A beginner-friendly plant</li>
                         <li>&#10004; Plant Care Guide: Detailed instructions on how to care for the plant</li>
                         <li>&#10004; Basic Gardening Tools: small trowel, pruning shears, and a watering can</li>
@@ -61,21 +62,23 @@
                 </div>
             </div>
 
-            <div class="col-5 plan-information-2">
-                <div class="subs-list">
+            <div class="col-5 plan-information-2 p-0 m-0">
+                <div class="subs-list px-5">
                     <div class="plan-information-title-2">
-                        <p class="planty-text-content">expand your collection and knowledge with a <br> variety of plants and advanced resources.</p>
+                        <p class="planty-text-sentence ">expand your collection and knowledge with a <br> variety of plants and advanced resources.</p>
                     </div>
 
-                    <ul>
-                <li>&#10004; Monthly Plant Delivery: A beginner-friendly plant</li>
-                <li>&#10004; Plant Care Guide: Detailed instructions on how to care for the plant</li>
-                <li>&#10004; Basic Gardening Tools: small trowel, pruning shears, and a watering can</li>
-                <li>&#10004; Exclusive Online Access: Access to a library of tutorial videos</li>
-                <li>&#10004; Eco-Friendly Packaging</li>
-                <li>&#10004; Seasonal Tips</li>
-                <li>&#10004; Surprise Gifts: rare plant seeds, stylish plant pots, or unique accessories.</li>
+                <ul class="planty-text-sentence ">
+                    <li>&#10004; Monthly Plant Delivery: A beginner-friendly plant</li>
+                    <li>&#10004; Plant Care Guide: Detailed instructions on how to care for the plant</li>
+                    <li>&#10004; Basic Gardening Tools: small trowel, pruning shears, and a watering can</li>
+                    <li>&#10004; Exclusive Online Access: Access to a library of tutorial videos</li>
+                    <li>&#10004; Eco-Friendly Packaging</li>
+                    <li>&#10004; Seasonal Tips</li>
+                    <li>&#10004; Surprise Gifts: rare plant seeds, stylish plant pots, or unique accessories.</li>
                 </ul>
+
+                
                 </div>
             </div>
             </div>
@@ -88,9 +91,9 @@
           <div id="accordionPanelsStayOpenExample" class="accordion">
                 @foreach($accordions as $key => $accordion)
                 <div class="accordion-item">
-                    <h2 class="accordion-header" id="flush-heading{{ $key + 1 }}">
+                    <h2 class="planty-heading-2 accordion-header" id="flush-heading{{ $key + 1 }}">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{ $key + 1 }}" aria-expanded="false" aria-controls="flush-collapse{{ $key + 1 }}">
-                            <div class="planty-heading-4-subs">{{ $accordion['title'] }}</div>
+                            <div class="planty-heading-4">{{ $accordion['title'] }}</div>
                         </button>
                     </h2>
                     <div id="flush-collapse{{ $key + 1 }}" class="accordion-collapse collapse" aria-labelledby="flush-heading{{ $key + 1 }}" data-bs-parent="#accordionFlushExample">
--- planty/routes/web.php
+++ planty/routes/web.php
@@ -3,73 +3,18 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleriesController;
+use App\Http\Controllers\BenefitController;
+use App\Http\Controllers\AccordionController;
 
 Route::get('/', function () {
     return view('about_us');
 });
 
-Route::get('/fun', function () {
-     $benefits = [
-    [
-        'emot' => '&#128516;',
-        'title' => 'Reduce Stress',
-        'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text.',
-    ],
-    [
-        'emot' => '&#128578;',
-        'title' => 'Improve Mood',
-        'desc' => 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum.',
-    ],
-    [
-        'emot' => '&#128151;',
-        'title' => 'Enhance Creativity',
-        'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text since the 1500s.',
-    ],
-    [
-        'emot' => '&#128640;',
-        'title' => 'Boost Productivity',
-        'desc' => 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Fusce suscipit libero eget elit fermentum, a pharetra quam luctus.',
-    ],
-    [
-        'emot' => '&#127793;',
-        'title' => 'Improve Air Quality',
-        'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text for centuries.',
-    ],
-    [
-        'emot' => '&#128591;',
-        'title' => 'Increase Mindfulness',
-        'desc' => 'Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula tellus nec turpis interdum, a suscipit nisl sollicitudin.',
-    ],
-];
+Route::get('/fun', [BenefitController::class, 'benefit']);
+Route::get('/subs', [AccordionController::class, 'accordion']);
 
-    return view('funfact',compact("benefits"));
-});
 
-Route::get('/subs', function () {
-    $accordions = [
-     [
-        'title' => 'What is included in each monthly plant kit?',
-        'content' => 'Each monthly plant kit includes a selection of seasonal plants, essential gardening tools, and accessories tailored to the current season. You\'ll also receive detailed care guides and access to exclusive online content, including tutorial videos and expert tips.'
-    ],
-     [
-        'title' => 'How do I choose the right plant kit for me?',
-        'content' => 'The best plant kit for you depends on your gardening experience and preferences. Here\'s a quick guide to help you decide: ...'
-    ],
-    [
-        'title' => 'How often will I receive a plant kit?',
-        'content' => 'You\'ll receive a new plant kit every month unless you pause or cancel your subscription. We offer flexible subscription options to fit your needs.'
-    ],
-    [
-        'title' => 'How do I care for my plants?',
-        'content' => 'Each plant kit includes detailed care guides specific to the plants you receive. You\'ll also have access to exclusive online resources, including tutorial videos and expert tips, to help you keep your plants thriving.'
-    ],
-    [
-        'title' => 'What happens if my plant arrives damaged?',
-        'content' => 'We take pride in carefully packaging our plants to ensure they arrive safely. However, if your plant arrives damaged, please contact us within 48 hours of receiving your kit and we\'ll be happy to replace it for free.'
-    ]
-];
-    return view('subscription',compact("accordions"));
-});
+
 
 Route::get('/subs/details', function () {
     return view('productdetail');
