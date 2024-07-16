973fde2b1307a7e0ce22dba46b734fc4f78bf233
Tasiaaaaa <anastasiam4002@gmail.com>
2024-07-16 18:08:04 +0700

    Payment Page and Product Page

--- planty/routes/web.php
+++ planty/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubsCategoriesController;
+use App\Http\Controllers\TransactionsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleriesController;
 
@@ -109,6 +110,7 @@ Route::get('/productBeginner', [SubsCategoriesController::class, 'productBeginne
 
 Route::get('/productEnthusiast', [SubsCategoriesController::class, 'productEnthusiast'])->name('productEnthusiast');
 
-Route::get('/paymentDetail', function () {
-    return view('payment_detail');
-})->name('paymentDetail');
+Route::post('/paymentDetail', [TransactionsController::class, 'processPayment'])->name('paymentDetail');
+Route::get('/paymentDetail', [TransactionsController::class, 'checkout'])->name('checkout');
+Route::get('/paymentDetail/success/{transaction}', [TransactionsController::class, 'success'])->name('checkout-success');
+Route::get('/paymentDetail/failed/{transaction}', [TransactionsController::class, 'failed'])->name('checkout-failed');  
