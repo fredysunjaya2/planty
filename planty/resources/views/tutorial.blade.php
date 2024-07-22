96af340d67f9e884ca04ea6874febd9d4834501b
kenzzhie <123430287+kenzzhie@users.noreply.github.com>
2024-07-22 13:38:40 +0700

    tutorial popup compenent

--- planty/resources/views/tutorial.blade.php
+++ planty/resources/views/tutorial.blade.php
@@ -42,35 +42,12 @@
                 <h4>Don't be afraid to ask us</h4>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
                 <button type="button" class="btn btn-primary">Base class</button>
-            </div>  
-        </div>
-    </section>    
-  </section>
-
-  <div class="modal fade modal-xl" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content popup d-flex">
-            <div class="popup-content d-flex flex-row">
-                <div class="popup-left d-flex flex-column">
-                    <div class="popup-vid">
-                        <iframe src="https://www.youtube.com/embed/HPJKxAhLw5I" title="Planting 20,000,000 Trees, My Biggest Project Ever!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
-                    <div class="popup-head d-flex flex-column">
-                        <div class="popup-title d-flex flex-row">
-                            <h2>Category Name</h2>
-                            <img src="../assets/tutorial/popup.png" alt="#">
-                        </div>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown </p>
-                    </div>
-                </div>
-                <div class="popup-right d-flex flex-column">
-                    <h2>Step by Step</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum h</p>
-                </div>
             </div>
         </div>
-    </div>
-    </div>
+    </section>
+</section>
+
+<x-modal-tutor-popup ></x-modal-tutor-popup>
 
 
 </section>
